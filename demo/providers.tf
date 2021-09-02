902a4ff669d3718da3f88d9c2101bed0ebc232cb
aspatil919 <68045526+aspatil919@users.noreply.github.com>
2021-09-02 14:25:46 +0530

    Update providers.tf

--- demo/providers.tf
+++ demo/providers.tf
@@ -1,6 +1,11 @@
 
 
 provider "azurerm" {
+  
+  subscription_id = var.var_subscription_id
+    client_id = var.var_client_id
+    tenant_id = var.var_tenant_id
+    client_secret = var.var_client_secret
   features {}
 }
 
@@ -9,10 +14,7 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "2.50.0"
-      subscription_id = var.var_subscription_id
-    client_id = var.var_client_id
-    tenant_id = var.var_tenant_id
-    client_secret = var.var_client_secret
+      
     }
   }
 }
