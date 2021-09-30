8992698baf396b503d8b984e10df03590cd09a9e
aspatil919 <68045526+aspatil919@users.noreply.github.com>
2021-09-30 13:10:47 +0530

    Update main.tf

--- demo/main.tf
+++ demo/main.tf
@@ -1,30 +1,35 @@
-
-
-
-
-
-
 terraform {
-    backend "azurerm" {
-        resource_group_name = "demo001"
-        storage_account_name = "storagev100"
-        container_name = "democon"
-        key = "main.tfstate"
-    }
+  backend "local" {
+    path = "This PC/Downloads/terraform.tfstate"
+  }
 }
+
+
+
+
+
+#terraform {
+ #   backend "azurerm" {
+  #      resource_group_name = "demo001"
+   #     storage_account_name = "storagev100"
+    #    container_name = "democon"
+     #   key = "main.tfstate"
+    #}
+#}
 
 resource "azurerm_resource_group" "RG" {
     name     = var.var_RG_name
     location = var.var_location
 }
 
-resource "azurerm_storage_account" "STG" {
-   name = var.var_Storage_name
-   resource_group_name = azurerm_resource_group.RG.name
-   location = var.var_location
-   account_tier = var.var_account_tier
-   account_replication_type = var.var_account_replication_type
- }
+#resource "azurerm_storage_account" "STG" {
+ #  name = var.var_Storage_name
+  # resource_group_name = azurerm_resource_group.RG.name
+   #location = var.var_location
+   #account_tier = var.var_account_tier
+   #account_replication_type = var.var_account_replication_type
+ 
+#}
 #resource "azurerm_storage_container" "storage_container" {
 #  name                  = var.var_storage_containername
  # storage_account_name  = azurerm_storage_account.STG.name
